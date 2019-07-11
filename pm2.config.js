cd7a242491a51d851f4ed45200563032f3b0e558
makan <makan@staff.sina.com.cn>
2019-07-11 17:06:16 +0800

    update package.json

---
+++ pm2.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'tool',
+      script: './dist/index.js',
+      instances: '1',
+      vizion: true,
+      env: {
+        PORT: '18002',
+        NODE_ENV: 'prod',
+        DEBUG: false
+      }
+    }
+  ]
+};
